c3ba2b037bb93eaa57f1c6fbea5ce07ab88aa223
kimbo <qhdwn25@gmail.com>
2023-05-12 12:35:02 +0900

    앤티티 수정

---
+++ Web/src/main/java/Board/BoardRepository.java
@@ -0,0 +1,13 @@
+package Board;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class BoardRepository {
+	@Id
+}
---
+++ Web/src/main/java/calendar/CalendarService.java
@@ -0,0 +1,11 @@
+package calendar;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CalendarService {
+
+}
