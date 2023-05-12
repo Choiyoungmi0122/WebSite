664ede7bd7509e19daab7c715d7af93ddb4006bd
kimbo <qhdwn25@gmail.com>
2023-05-12 15:46:09 +0900

    h

--- Web/src/main/java/Table/Calender.java
+++ Web/src/main/java/Table/Calender.java
@@ -1,5 +1,25 @@
 package Table;
 
-public class Calender {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Calender {
+	@Id
+	private Integer Calender_Id;
+	
+	private String Student_Id;
+	private LocalDateTime Calender_Register;
+	private String Calender_Text;
+	private LocalDateTime Calender_Deadline;
+	private String Calender_Category;
+	private String Calender_Replay;
+	@OneToOne
 }
