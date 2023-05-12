5a2d3d5a1f06a2d07294e6ea22dcf81b13c7ce8d
choiyoungmi2252@gmail.com <choiyoungmi2252@gmail.com>
2023-05-12 13:23:03 +0900

    수정실

--- Web/src/main/java/Board/BoardRepository.java
+++ Web/src/main/java/Board/BoardRepository.java
@@ -1,7 +1,21 @@
 package Board;
 
 import jakarta.persistence.Entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,4 +24,8 @@ import lombok.Setter;
 @Entity
 public class BoardRepository {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length=)
 }
--- Web/src/main/java/calendar/CalendarRepository.java
+++ Web/src/main/java/calendar/CalendarRepository.java
@@ -2,4 +2,17 @@ package calendar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CalendarRepository extends JpaRepository<Calender, Integer>
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import lombok.Setter;
+
+@Setter
+public interface CalendarRepository extends JpaRepository<Calender, Integer>{
+	Calneder 
+}
