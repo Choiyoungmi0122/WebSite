9fe42e9861dc451e1c61b83aa187a57c191dc218
ssw0420 <tmddn5645@gmail.com>
2023-05-19 22:49:16 +0900

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

--- Web/src/main/java/com/example/demo/table/LikeLanguage.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.table;
-
-public class LikeLanguage {
-
-}
--- Web/src/main/java/com/example/demo/table/NoticeComment.java
+++ Web/src/main/java/com/example/demo/table/NoticeComment.java
@@ -1,5 +1,37 @@
 package com.example.demo.table;
 
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class NoticeComment {
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private Integer Com_Id;
+	
+	@ManyToOne
+	@JoinColumn(name="NOTICE_ID")
+	private Notice notice;
+	
+	// @ManyToOne (mappedBy = "UserInfo")
+	// private Integer Student_Id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String Com_Text;
+	
+	private LocalDateTime Com_Register;
 
 }
