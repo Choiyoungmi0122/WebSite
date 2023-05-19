3520a6801174e60e6a4f919426b6e13d469fa34b
choiyoungmi2252@gmail.com <choiyoungmi2252@gmail.com>
2023-05-19 21:21:06 +0900

    버전1 초기

--- Web/src/main/java/com/example/demo/calender/CalenderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.calender;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.table.Calender;
-
-public interface CalenderRepository extends JpaRepository<Calender, Integer>{
-	
-}
