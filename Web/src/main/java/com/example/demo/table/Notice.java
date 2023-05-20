a8c2eaeae50812b87a810a47d107e0fce80b9b27
ssw0420 <tmddn5645@gmail.com>
2023-05-20 16:43:16 +0900

    Notice Entity 수정본

--- Web/src/main/java/com/example/demo/table/Notice.java
+++ Web/src/main/java/com/example/demo/table/Notice.java
@@ -27,9 +27,9 @@ public class Notice {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer Notice_Id;
 	
-//	@ManyToOne
-//	@JoinColumn(name="STUNDENT_ID")
-//	private Userinfo userinfo;
+	@ManyToOne
+	@JoinColumn(name="STUNDENT_ID")
+	private Userinfo userinfo;
 
 	@Column(length = 200)
 	private String Notice_Title;
