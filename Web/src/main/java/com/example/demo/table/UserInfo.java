afa9b4f49bcc14b3637620d86cb6144339ac3afd
qrstu <qrstu@kimhyun>
2023-05-23 13:26:17 +0900

    사용자 엔티티

--- Web/src/main/java/com/example/demo/table/UserInfo.java
+++ Web/src/main/java/com/example/demo/table/UserInfo.java
@@ -1,13 +1,24 @@
 package com.example.demo.table;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 
 
 public class UserInfo {
-
+	@Id
+	@Column(unique = true)
+	private String sid;
+	
+	private String sname;
+	
+	private String password;
+	
+	@Column(unique = true)
+	private String semail;
 	
 	//Calender에 PK 쓰기위해 선언
 	@OneToMany(mappedBy="UserInfo")
