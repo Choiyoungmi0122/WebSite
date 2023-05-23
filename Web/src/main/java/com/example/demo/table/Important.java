142c54af726e79928cb2004c2af4494ed1406f6e
choiyoungmi2252@gmail.com <choiyoungmi2252@gmail.com>
2023-05-23 12:24:29 +0900

    수

--- Web/src/main/java/com/example/demo/table/Important.java
+++ Web/src/main/java/com/example/demo/table/Important.java
@@ -6,19 +6,26 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 public class Important {
-	private Long Student_Id;		//작성자 pk
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Impor_Id;
+	
+	@ManyToOne
+	@JoinColumn(name="STUNDENT_ID")  //작성자 pk
+	private Userinfo userinfo;
+	
 	
     @Column(length = 200)	//제목 
     private String Impor_Title;
     
     @Column(columnDefinition = "TEXT")   //내용 
     private String Impor_Text;
+    
     private LocalDateTime Impor_Register;	//작성일시 
     private Boolean Important;		//필독 여부 
 	
