885a4a830403259d94684fb732c730a5b39e03e0
ssw0420 <tmddn5645@gmail.com>
2023-05-24 21:29:57 +0900

    .

---
+++ Web/src/main/java/com/example/demo/notice/NoticeService.java
@@ -0,0 +1,32 @@
+package com.example.demo.notice;
+
+import com.example.demo.share.*;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.table.Notice;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@Service
+public class NoticeService {
+	
+	private final NoticeRepository noticeRepository;
+	
+	public List<Notice> getList(){
+		return this.noticeRepository.findAll();
+	}
+	
+	public Notice getNotice(Integer id) {
+		Optional<Notice> notice = this.noticeRepository.findById(id);
+		if (notice.isPresent()) {
+			return notice.get();
+		} else {
+			throw new DataNotFoundException("Notice not found");
+		}
+	}
+}
