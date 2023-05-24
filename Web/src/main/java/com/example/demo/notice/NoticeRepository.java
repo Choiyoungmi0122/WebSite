1ab35c0cd9a544681c5fbabff6d992b8a86e6581
ssw0420 <tmddn5645@gmail.com>
2023-05-24 21:28:25 +0900

    .

---
+++ Web/src/main/java/com/example/demo/notice/NoticeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.table.Notice;
+
+public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+	Notice findByNoticeTitle(String Notice_Title);
+//	Notice findByNoticeTitleAndNoticeText(String Notice_Title, String Notice_Text);
+}
