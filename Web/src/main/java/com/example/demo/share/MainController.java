9cf5b7869cd9fa75f4bf0bea2420d2599d037476
qrstu <qrstu@kimhyun>
2023-05-25 19:52:36 +0900

    test

--- Web/src/main/java/com/example/demo/share/MainController.java
+++ Web/src/main/java/com/example/demo/share/MainController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// @Controller 애너테이션을 적용하면 현재 클래스가 스프링부트의 컨트롤러가 됨
 @Controller
 public class MainController {
-
-    @GetMapping("/sbb")
-    @ResponseBody
-    public String index() {
-        return "index";
+    @GetMapping("/")
+    public String root() {
+    	return "home";
     }
 }
