aec29998b1ad6af84d24bbbc28d70e5b77c85369
ssw0420 <tmddn5645@gmail.com>
2023-05-25 21:33:27 +0900

    .

--- Web/src/main/java/com/example/demo/table/Notice.java
+++ Web/src/main/java/com/example/demo/table/Notice.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
