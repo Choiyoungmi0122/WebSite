5deb08d04e3ad23df17c2a9a705ab0c803efd0f1
902MHz <alsrud7578@naver.com>
2023-06-11 20:45:53 +0900

    캘린더 등록 추

--- Web/src/main/java/com/example/demo/calender/CalenderController.java
+++ Web/src/main/java/com/example/demo/calender/CalenderController.java
@@ -1,14 +1,10 @@
 package com.example.demo.calender;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-
 import java.security.Principal;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +16,9 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.table.Calender;
 import com.example.demo.table.UserInfo;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/calender")
 @RequiredArgsConstructor
