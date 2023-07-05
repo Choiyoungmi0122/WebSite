89024c120eff41cb680c7efb1712f4b6a87f087d
kimbo <kimbo@why_not>
2023-07-05 11:36:44 +0900

    .

--- Web/src/main/java/com/example/demo/board/ImportantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImportantRepository extends JpaRepository<Important, Integer> {
-//    Important findBytitleandtext(String title, String text);
-    // List<Important> findBytitleLike(String title);
-    Important findByImpoTitle(String title);
-}
--- Web/src/main/java/com/example/demo/board/NoticeCommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeCommentRepository extends JpaRepository<NoticeComment, Integer> {
-		
-}
