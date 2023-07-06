9b1b771633ea1924498c7f720052e8525a4e7666
kimbongju02 <qhdwn25@gmail.com>
2023-07-06 20:17:15 +0900

    reset

--- insight/src/main/java/com/version1/insight/board/NoticeCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.version1.insight.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeCommentRepository extends JpaRepository<NoticeComment, Integer> {
-
-}
